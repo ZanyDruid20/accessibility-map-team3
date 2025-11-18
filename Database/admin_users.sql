f243244fe077c17b3cb24c2372fb7d6d8293b77a
Kaila <143640143+KailaHos@users.noreply.github.com>
2025-11-17 21:13:20 -0500

    Update admin_users.sql

--- Database/admin_users.sql
+++ Database/admin_users.sql
@@ -2,5 +2,5 @@ USE campus_map;
 CREATE TABLE Admin (
     admin_id INT AUTO_INCREMENT PRIMARY KEY,
     username VARCHAR(100) UNIQUE NOT NULL,
-    password_hash VARCHAR(255) NOT NULL
+    password VARCHAR(255) NOT NULL
 )
